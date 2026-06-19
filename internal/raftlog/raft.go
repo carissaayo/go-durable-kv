76c6241231a132e5a7320edaa0707680170403cc
Ajao Yussuf <ajaoyussufayo@gmail.com>
2026-06-19 11:50:13 +0100

    ok

--- internal/raftlog/raft.go
+++ internal/raftlog/raft.go
@@ -2,7 +2,10 @@ package raftlog
 
 import (
 	"bufio"
+	"fmt"
+	"hash/crc32"
 	"os"
+	"path/filepath"
 
 	"github.com/carissaayo/go-durable-kv/internal/engine"
 )
@@ -16,4 +19,23 @@ type RaftLog struct {
 	nextOffset int64 // byte offset where the next record will start
 }
 
-func OpenRaftLog(path string, syncPolicy engine.SyncPolicy) (*RaftLog, error)
+const (
+	lenSize    = 4
+	crcSize    = 4
+	maxPayload = 64 << 20 // 64 MiB
+)
+
+// crcTable is computed once at init time using the Castagnoli polynomial,
+// which has better error-detection properties than the IEEE polynomial.
+var crcTable = crc32.MakeTable(crc32.Castagnoli)
+
+func OpenRaftLog(path string, syncPolicy engine.SyncPolicy) (*RaftLog, error) {
+	dir := filepath.Dir(path)
+
+	if err := os.MkdirAll(dir, 0o755); err != nil {
+		return nil, fmt.Errorf("create raft dir %q: %w", dir, err)
+	}
+
+	return nil, nil
+
+}
